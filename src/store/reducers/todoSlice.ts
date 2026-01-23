cca92082505215af356ec90e7484d081a34ccedd
Igor <golovkovviggreserv@gmail.com>
2026-01-23 06:54:37 +0300

    add: работа с redux

---
+++ src/store/reducers/todoSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+type todoSlice = {
+};
+
+const initialState: todoSlice = {
+};
+
+
+const todoSlice = createSlice({
+  name: 'feedSlice',
+  initialState,
+  reducers: {
+  },
+  extraReducers: (builder) => {
+
+  }
+});
+
+export const {} = todoSlice.actions;
+
+export default todoSlice
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import todoReducer from './reducers/todoSlice'
 
+const rootReducer = combineReducers({
+    todo: todoReducer
+});
 export const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
 })
 
 
